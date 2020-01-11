dbbfee3427fbe32e17cb7cf33b512bfbae899907
tejas sharma <tejassharma.softvan@gmail.com>
2020-01-11 11:31:43 +0530

    add identity

---
+++ FarmEasy/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FarmEasy.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FarmEasy.Areas.Identity.IdentityHostingStartup))]
+namespace FarmEasy.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
